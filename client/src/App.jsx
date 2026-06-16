3643a58b8e186cae097de3be59f4e1bd0d8c25df
Muneeb <syedmuneebmalik1827@gmail.com>
2026-06-16 23:09:18 +0530

    Setup vite and tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className='text-red-500'>Hello</p>
+      </div>
+    </>
+  )
+}
+
+export default App
